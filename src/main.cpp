42ac5c944265917c8e3312ca200779960a594d3b
Oztoprak <oguz.oztoprak@tum.de>
2018-10-03 23:31:51 +0200

    more cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 	std::shared_ptr<octree> o = std::make_shared<octree>(0.0,1.0,0.0,1.0,0.0,1,0,std::weak_ptr<octree>());
 	o->generateQuadTree(max_level);
 	o->showVtk(o->getAllPointsDeepestLevel(max_level));
-	o->showVtk(o->getAllPoints(max_level));
+//	o->showVtk(o->getAllPoints(max_level));
 
 
 	return 0;

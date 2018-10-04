bb5724760b095b4328f54c9a4d042e612ece7428
Oztoprak <oguz.oztoprak@tum.de>
2018-10-04 09:03:05 +0200

    changed function names

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,8 @@ int main(int argc, char *argv[])
 
 	std::shared_ptr<octree> o = std::make_shared<octree>(0.0,1.0,0.0,1.0,0.0,1,0,std::weak_ptr<octree>());
 	o->generateQuadTree(max_level);
-	o->showVtk(o->getAllPointsDeepestLevel(max_level));
+//	o->WriteUnstrucredGrid("sphere.vtu");
+	o->showAll(o->getAllPointsDeepestLevel());
 //	o->showVtk(o->getAllPoints(max_level));
 
 
